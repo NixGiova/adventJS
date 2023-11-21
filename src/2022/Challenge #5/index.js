cd55534fcc7065b5583bec39c7e1d66b2e44e23d
Nixon Moreno <nixon.moreno@etc-latam.com>
2023-11-21 16:56:03 -0500

    Optimized Challenge 5

--- src/2022/Challenge #5/index.js
+++ src/2022/Challenge #5/index.js
@@ -13,28 +13,63 @@ function getMaxGifts(giftsCities, maxGifts, maxCities) {
 
   let result = []
   for (let j = maxCities; j > 0; j--) {
-    let resultIter = 0
     getCombinations([], 0, j)
-    combinations.forEach((comb) => {
-      const sum = comb.reduce((a, b) => a + b, 0)
-      if (sum > 0 && sum <= maxGifts && sum > resultIter) resultIter = sum
-    })
-    result = [...result, resultIter]
+    console.log(combinations)
+    const iter = combinations.reduce((sum, comb) => {
+      const sumIter = comb.reduce((a, b) => a + b, 0)
+      return sumIter <= maxGifts && sumIter > sum ? sumIter : sum
+    }, 0)
+    result = [...result, iter]
     if (result === maxGifts) return
     combinations = []
   }
 
-  return result.reduce((acc, current) => {
-    return current > 0 && current <= maxGifts && current > acc ? current : acc
-  }, 0)
+  const max = result.reduce((a, b) => Math.max(a, b), -Infinity)
+  return max
 }
 
-console.log(getMaxGifts([12, 3, 11, 5, 7], 20, 3)) // 20
-console.log(getMaxGifts([50], 15, 1)) // 0
-console.log(getMaxGifts([50], 100, 1)) // 50
-console.log(getMaxGifts([50, 70], 100, 1)) // 70
-console.log(getMaxGifts([50, 70, 30], 100, 2)) // 100
-console.log(getMaxGifts([50, 70, 30], 100, 3)) // 100
-console.log(getMaxGifts([50, 70, 30], 100, 4)) // 100
-console.log(getMaxGifts([50, 10, 40, 1000, 500, 200], 199, 4)) // 100
-console.log(getMaxGifts([50, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 1000, 1000)) // 115
+const getMaxGiftsOptimizer = (giftsCities, maxGifts, maxCities) => {
+  let combinations = []
+  combinations.push([], [giftsCities[0]])
+  giftsCities.shift()
+  giftsCities.map((element) => {
+    const newList = combinations.map((comb) => {
+      let _combination = [...comb]
+      if (_combination.length < maxCities) {
+        _combination.push(element)
+      }
+      return _combination
+    })
+    combinations = combinations.concat(newList)
+  })
+
+  combinations.shift()
+  return Math.max(
+    ...combinations.map((x) => {
+      let sum = x.reduce((total, num) => total + num)
+      return sum > maxGifts ? 0 : sum
+    })
+  )
+}
+
+console.log(getMaxGiftsOptimizer([12, 3, 11, 5, 7], 20, 3)) // 20
+console.log(getMaxGiftsOptimizer([50], 15, 1)) // 0
+console.log(getMaxGiftsOptimizer([50], 100, 1)) // 50
+console.log(getMaxGiftsOptimizer([50, 70], 100, 1)) // 70
+console.log(getMaxGiftsOptimizer([50, 70, 30], 100, 2)) // 100
+console.log(getMaxGiftsOptimizer([50, 70, 30], 100, 3)) // 100
+console.log(getMaxGiftsOptimizer([50, 70, 30], 100, 4)) // 100
+console.log(getMaxGiftsOptimizer([50, 10, 40, 1000, 500, 200], 199, 4)) // 100
+console.log(
+  getMaxGiftsOptimizer([50, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 1000, 1000)
+) // 115
+
+// console.log(getMaxGifts([12, 3, 11, 5, 7], 20, 3)) // 20
+// console.log(getMaxGifts([50], 15, 1)) // 0
+// console.log(getMaxGifts([50], 100, 1)) // 50
+// console.log(getMaxGifts([50, 70], 100, 1)) // 70
+// console.log(getMaxGifts([50, 70, 30], 100, 2)) // 100
+// console.log(getMaxGifts([50, 70, 30], 100, 3)) // 100
+// console.log(getMaxGifts([50, 70, 30], 100, 4)) // 100
+// console.log(getMaxGifts([50, 10, 40, 1000, 500, 200], 199, 4)) // 100
+// console.log(getMaxGifts([50, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 1000, 1000)) // 115
