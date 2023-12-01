08cc2e6f9ac6119da4d43e60c614956b34041710
Nixon Moreno <nixon.moreno@etc-latam.com>
2023-12-01 12:27:28 -0500

    Second function with filter solution

--- src/2023/Challenge #1/index.js
+++ src/2023/Challenge #1/index.js
@@ -7,6 +7,11 @@ function findFirstRepeated(gifts) {
   return -1
 }
 
+function findFirstRepeatedFilter(gifts) {
+  const repeated = giftIds.filter((gift, i) => giftIds.indexOf(gift) !== i)
+  return repeated.length > 0 ? repeated[0] : -1
+}
+
 const giftIds = [2, 1, 3, 5, 3, 2]
 const firstRepeatedId = findFirstRepeated(giftIds)
 console.log(firstRepeatedId) // 3
@@ -21,3 +26,5 @@ console.log(firstRepeatedId2) // -1
 const giftIds3 = [5, 1, 5, 1]
 const firstRepeatedId3 = findFirstRepeated(giftIds3)
 console.log(firstRepeatedId3) // 5
+
+console.log(findFirstRepeatedFilter(giftIds)) // 3
