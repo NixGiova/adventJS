eb66601b2cc08abf793de661888228d31c02531a
Nixon Moreno <nixon.moreno@etc-latam.com>
2023-12-01 10:45:12 -0500

    Challenge #1 2023

---
+++ src/2023/Challenge #1/index.js
@@ -0,0 +1,23 @@
+function findFirstRepeated(gifts) {
+  const seen = {}
+  for (const id of gifts) {
+    if (seen[id]) return id
+    seen[id] = true
+  }
+  return -1
+}
+
+const giftIds = [2, 1, 3, 5, 3, 2]
+const firstRepeatedId = findFirstRepeated(giftIds)
+console.log(firstRepeatedId) // 3
+// Even though 2 and 3 are repeated
+// 3 appears second time first
+
+const giftIds2 = [1, 2, 3, 4]
+const firstRepeatedId2 = findFirstRepeated(giftIds2)
+console.log(firstRepeatedId2) // -1
+// It is -1 since no number is repeated
+
+const giftIds3 = [5, 1, 5, 1]
+const firstRepeatedId3 = findFirstRepeated(giftIds3)
+console.log(firstRepeatedId3) // 5
