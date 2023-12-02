292b8874a6ebe432de49733082f30b451debd689
Nixon Moreno <nixon.moreno@etc-latam.com>
2023-12-02 17:46:11 -0500

    Challenge " 2 2023 Alternative Solutions

--- src/2023/Challenge #2/index.js
+++ src/2023/Challenge #2/index.js
@@ -1,8 +1,16 @@
 function manufacture(gifts, materials) {
-  const madeGifts = gifts.filter((gift) =>
+  return gifts.filter((gift) =>
     gift.split('').every((letter) => materials.includes(letter))
   )
-  return madeGifts
+}
+
+function manufactureExp1(gifts, materials) {
+  const regex = new RegExp(`^[${materials}]*$`)
+  return gifts.filter((gift) => regex.test(gift))
+}
+function manufactureExp2(gifts, materials) {
+  const regex = new RegExp(`^[${materials}]+${'$'}`)
+  return gifts.filter(regex.test.bind(regex))
 }
 
 console.log(manufacture(['tren', 'oso', 'pelota'], 'tronesa')) // ["tren", "oso"]
