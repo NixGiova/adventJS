dbf6bde8557e9b9e686967c36660058c7e1b816a
Nixon Moreno <nixon.moreno@etc-latam.com>
2023-12-06 16:32:47 -0500

    Challenge #3 Optimized Code

--- src/2023/Challenge #3/index.js
+++ src/2023/Challenge #3/index.js
@@ -8,6 +8,17 @@ function findNaughtyStep(original, modified) {
   return modified.slice(original.length)
 }
 
+function findNaughtyStepOpt(original, modified) {
+  if (original.length === modified.length) return ''
+  const iterator = modified.length > original.length ? modified : original
+  for (let index = 0; index < iterator.length; index++) {
+    if (original[index] !== modified[index]) {
+      return iterator[index]
+    }
+  }
+  return modified.slice(original.length)
+}
+
 console.log(findNaughtyStep('abcd', 'abcde')) // 'e
 console.log(findNaughtyStep('stepfor', 'stepor')) // 'f'
 console.log(findNaughtyStep('abcde', 'abcde')) // ''
